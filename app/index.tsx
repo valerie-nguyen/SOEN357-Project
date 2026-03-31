ac2ce7b0b03b2bfd68ad4d31729a160fbf8a75cc
valerie-nguyen <val.nguyen08@gmail.com>
2026-03-31 00:15:55 -0400

    display map

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function Index() {
+export default function MapScreen() {
+  const INITIAL_REGION = {
+    latitude: 45.4971,
+    longitude: -73.5791,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        //showsPointsOfInterest={false}
+        userInterfaceStyle="light"
+        initialRegion={INITIAL_REGION}
+      >
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { width: '100%', height: '100%' },
+});
